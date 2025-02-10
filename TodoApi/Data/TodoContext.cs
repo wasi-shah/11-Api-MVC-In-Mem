bfe252b1613a3d739f09fe9f05e51d83cdf9e561
wasi-shah <51749920+wasi-shah@users.noreply.github.com>
2025-02-10 16:08:10 +0000

    Update

---
+++ TodoApi/Data/TodoContext.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+}
+
